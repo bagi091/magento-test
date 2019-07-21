7e64d4c3a5297a53e636058cf21ceeaa10855736
bagi091 <bagi091@gmail.com>
2019-07-21 13:47:32 +0300

    TEST-1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Magento 2 - demo theme
+
+1. Theme based on Magento2 Blank. In current case using Luma is unnecessary dependency.
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Vaimo. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Vaimo/test', __DIR__);
